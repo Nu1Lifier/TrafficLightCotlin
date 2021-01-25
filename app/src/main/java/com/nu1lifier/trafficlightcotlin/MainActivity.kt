fe7575ece8b99fadaaef9dc1e330918579b53bd6
George <64532466+Nu1Lifier@users.noreply.github.com>
2021-01-25 21:30:08 +0300

    is done

---
+++ app/src/main/java/com/nu1lifier/trafficlightcotlin/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.nu1lifier.trafficlightcotlin
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : Activity() {
+    var imSemafor:ImageView? = null
+    var counter:Int = 0
+    var timer:Timer? = null
+    var is_run:Boolean = false
+    var imageArray:IntArray = intArrayOf(R.drawable.semafor_red, R.drawable.semafor_yellow, R.drawable.semafor_green)
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imSemafor = findViewById(R.id.imSemafor)
+
+    }
+
+    fun onClickStartStop(view: View){
+        view as ImageButton
+
+        if (!is_run){
+            startStop()
+            view.setImageResource(R.drawable.button_stop)
+            is_run = true
+        } else{
+            imSemafor?.setImageResource(R.drawable.semafor_grey)
+            timer?.cancel()
+            view.setImageResource(R.drawable.button_start)
+            is_run = false
+            counter = 0
+
+
+        }
+
+
+
+    }
+
+    fun startStop(){
+        timer = Timer()
+        timer?.schedule(object: TimerTask(){
+            override fun run() {
+              runOnUiThread{
+                  imSemafor?.setImageResource(imageArray[counter])
+                  counter++
+              }
+                if (counter == 3) counter = 0
+            }
+
+        },0, 1000)
+    }
+}
